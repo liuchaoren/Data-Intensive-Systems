e09f8abd31bc42872d26b5480c5a44c6a06b9744
Brett Walenz <bwalenz@cs.duke.edu>
2015-03-24 04:38:59 +0000

    Updated to include pythonpath

--- p1-tests/vote_percent/run.sh
+++ p1-tests/vote_percent/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SPARK_HOME=~/spark
+export PYTHONPATH=/root/cs516-team
 
 $SPARK_HOME/bin/spark-submit --driver-memory 20g --master spark://ec2-23-20-3-44.compute-1.amazonaws.com:7077 vote_percent.py
 #$SPARK_HOME/bin/spark-submit --driver-memory 5g --master spark://ec2-54-152-24-8.compute-1.amazonaws.com:7077 vote_percent.py
